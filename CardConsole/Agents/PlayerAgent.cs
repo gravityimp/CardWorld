27a5f639ff2973cf073bedab750eab61703f0c99
Yurii Tyshchenko <55381248+gravityimp@users.noreply.github.com>
2024-06-20 00:35:19 +0200

    Added PlayerAgent - agent that listens to the player's input from console. Added test usage in Program.cs.

---
+++ CardConsole/Agents/PlayerAgent.cs
@@ -0,0 +1,23 @@
+﻿using CardConsole.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardConsole.Agents
+{
+    internal class PlayerAgent : Base.Agent
+    {
+        public override string Step(List<string> legalActions)
+        {
+            string action = "";
+            while (!legalActions.Contains(action))
+            {
+                Console.WriteLine($"Choose action {Utilities.ListToString(legalActions)}:");
+                action = Console.ReadLine();
+            }
+            return action;
+        }
+    }
+}
