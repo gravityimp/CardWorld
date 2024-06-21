879971d519f7620721feac19756e50dc94d8db35
Yurii Tyshchenko <55381248+gravityimp@users.noreply.github.com>
2024-06-21 16:25:17 +0200

    Added RandomAgent - agent that choose random action from legal actions.

---
+++ CardConsole/Agents/RandomAgent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardConsole.Agents
+{
+    internal class RandomAgent : Base.Agent
+    {
+        Random Random { get; init; }
+        
+        public RandomAgent(int? seed = null)
+        {
+            Random = seed.HasValue ? new Random(seed.Value) : new Random();
+        }
+
+        public override string Step(List<string> legalActions)
+        {
+            return legalActions[Random.Next(legalActions.Count())];
+        }
+    }
+}
--- CardConsole/Program.cs
+++ CardConsole/Program.cs
@@ -4,7 +4,7 @@ using CardConsole.Games.Blackjack;
 using CardConsole.Utils;
 using BlackjackGame = CardConsole.Games.Blackjack.Game;
 
-List<Agent> agents = new List<Agent>() { new PlayerAgent() };
+List<Agent> agents = new List<Agent>() { new PlayerAgent(), new RandomAgent() };
 var game = new BlackjackGame(agents, 1);
 var state = game.Start();
 Console.WriteLine(Utilities.DictionaryToString(state));
